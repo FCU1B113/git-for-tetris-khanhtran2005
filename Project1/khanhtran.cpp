ad8d8f1555e02f8f09c88d2be94f8fc125be15cb
khanh trandinh <trandinhkhanh10ay@gmail.com>
2025-04-24 11:55:22 +0800

    khanhtran.cpp

--- Project1/khanhtran.cpp
+++ Project1/khanhtran.cpp
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 typedef enum
 {
 	EMPTY = -1,
@@ -10,6 +11,7 @@ typedef enum
 	T,
 	Z
 }ShapeId;
+
 typedef enum
 {
 	RED = 41,
@@ -29,49 +31,110 @@ typedef struct
 	int size;
 	char rotates[4][4][4];
 }Shape;
+
 Shape shape[7] = {
 	{
 		.shape = I,
 		.color = CYAN,
 		.size = 4,
-		.totates =
-	{
+		.rotates =
 		{
-			{0,0,0,0},
-	        {1,1,1,1},
-	        {0,0,0,0},
-	        {0,0,0,0},
-	      {,
-		  }
-            {0,0,1,0},
-	        {0,0,1,0},
-	        {0,0,1,0},
-			{0,0,1,0},
-}
-}
-}
-}
-
-
-
-
+			{
+				{0,0,0,0},
+				{1,1,1,1},
+				{0,0,0,0},
+				{0,0,0,0},
+			},
+			{
+				{0,0,1,0},
+				{0,0,1,0},
+				{0,0,1,0},
+				{0,0,1,0},
+			},
+			{
+				{0,0,0,0},
+				{0,0,0,0},
+				{1,1,1,1},
+				{0,0,0,0},
+			},
+			{
+				{0,1,0,0},
+				{0,1,0,0},
+				{0,1,0,0},
+				{0,1,0,0},
+			}
+		}
+	},
+	{
+		.shape = J,
+		.color = BLUE,
+		.size = 3,
+	},
+	{
+		.shape = L,
+		.color = YELLOW,
+		.size = 3,
+	},
+	{
+		.shape = O,
+		.color = WHITE,
+		.size = 2,
+		.rotates = {
+			{
+				{1,1},
+				{1,1}
+			},
+			{
+				{1,1},
+				{1,1}
+			},
+			{
+				{1,1},
+				{1,1}
+			},
+			{
+				{1,1},
+				{1,1}
+			}
+		}
+	},
+	{
+		.shape = S,
+		.color = GREEN,
+		.size = 3,
+	},
+	{
+		.shape = T,
+		.color = PURPLE,
+		.size = 3,
+	},
+	{
+		.shape = Z,
+		.color = RED,
+		.size = 3,
+	},
+};
 
 int main() {
 	Color cur;
-	for (int i = 0; i < 7; i++) {
-		for (int r = 0; r < 4;r++) {
-			for (int s = 0; s < shape[i].size;s++) {
-				for (int t = 0; t < shape[i].size;t++) {
+	// 有幾種方塊
+	for (int i = 0; i < 1; i++) {
+		// 印出方塊樣式
+		for (int r = 0; r < 4; r++) {
+			for (int s = 0; s < shape[i].size; s++) {
+				for (int t = 0; t < shape[i].size; t++) {
 					if (shape[i].rotates[r][s][t] == 0) {
-						cur = white;
+						cur = WHITE;
 					}
 					else {
-						cur=
+						cur = shape[i].color;
 					}
+					printf("\033[%dm  \033[0m", cur);
 				}
+				printf("\n");
 			}
+			printf("\n");
 		}
 	}
-
-
+	return 0;
 }
